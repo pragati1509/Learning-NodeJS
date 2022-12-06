9d0b6a05990eeac7cf1e3852f491c22faba16115
pragati1509 <77267328+pragati1509@users.noreply.github.com>
2022-12-06 20:45:04 +0530

    basicExpress

--- nodeJS/nodeJS/basic_express.js
+++ nodeJS/nodeJS/basic_express.js
@@ -3,5 +3,13 @@ var express = require('express');   //module
 var app = express(); //service/function
 
 app.get("/",function(req,res){
-    res.send("Welcome");
+    res.send("I'm expressJS");
 });
+
+var server = app.listen(3000,function(){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log(host);
+    console.log(port);
+    console.log("application runs at http://",host,port);
+})
