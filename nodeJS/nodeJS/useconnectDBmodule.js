2331b2432dbceaa457616dbe2fdb1d5426ef1045
pragati1509 <77267328+pragati1509@users.noreply.github.com>
2022-12-06 22:11:25 +0530

    connectDB

--- nodeJS/nodeJS/useconnectDBmodule.js
+++ nodeJS/nodeJS/useconnectDBmodule.js
@@ -1,4 +1,13 @@
-const connectDB = require("./connectDB");
+const client = require("./connectDB");
 
-connectDB.client();
+const db = client.db("employee");
+
+db.collection("customers").find({}).toArray(function(err,res){
+    if(err)
+       throw err;
+
+    console.log(res);
+})
+
+
 
